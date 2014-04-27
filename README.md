7e691112b1c63ffa10ac58b7cbd0decf4e0afaf0
boisei0 <boisei0@hubsec.eu>
2014-04-27 13:47:39 +0200

    More UI. About the screw stuff up, so commit before :)

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
-# Autism-friendly study-agenda #
+# Autism-friendly personal organizer for educational usage #
 This project was made with the [Kivy Framework](http://kivy.org) for the [\#2014 Kivy App Contest](http://kivy.org/#contest)\.
-The application is an autism-friendly agenda for educational usage, but usable for spare time as well\.
-It is designed for Windows, Linux and Android. In the future, it should work on Mac OS X and iOS as well\.
+The application is an autism-friendly personal organizer for educational usage, but usable for spare time as well\.
+It is designed for Windows, Linux and Android. In the future, it should work on Mac OS X and iOS too\.
 
 ## Features ##
 * Every course has it's own colour
 * Plan your homework
 * Ability to turn on/off notification(s) per activity
-* Synchronize agendas with Dropbox
+* Synchronize your agenda on different devices with Dropbox
--- main.py
+++ main.py
@@ -2,25 +2,69 @@ import kivy
 kivy.require('1.8.0')
 
 from kivy.app import App
-from kivy.uix.widget import Widget
-from kivy.uix.dropdown import DropDown
+from kivy.config import ConfigParser
+from kivy.properties import ObjectProperty
 
-__author__ = 'boisei0'
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.dropdown import DropDown
+from kivy.uix.popup import Popup
+from kivy.uix.settings import Settings
+from kivy.uix.widget import Widget
+
+__author__ = 'Rob Derksen (boisei0)'
+__appname__ = 'AutiAgenda'
+
+config = ConfigParser()
+config.read('config/autiagenda.ini')
+
+settings = Settings()
+# TODO: Add settings / config later
 
 
 class Agenda(Widget):
+    selected_date = 'Today'
+    about_popup = Popup(title='About...', size_hint=(0.3, 0.5), auto_dismiss=False)
+
     def __init__(self, **kwargs):
         super(Agenda, self).__init__(**kwargs)
 
         self.top_menu_more = AgendaTopMenuDropDown(self)
+        self.selected_date_dropdown = SelectedDateDropDown(self)
+
+        self.about_popup.attach_to = self
+        self.about_popup.content = AboutDialog(root=self)
+
+        self.settings = settings
 
     def open_settings_dialog(self):
         print('settings')
+        settings.to_window(0, 0)
         self.top_menu_more.dismiss()
 
     def sync_all(self):
         print('sync')
         self.top_menu_more.dismiss()
+
+    def display_courses(self):
+        print('courses')
+        self.top_menu_more.dismiss()
+
+    def display_schedule(self):
+        print('schedule')
+        self.top_menu_more.dismiss()
+
+    def display_about(self):
+        self.about_popup.open()
+        self.top_menu_more.dismiss()
+
+    def display_next_day(self):
+        pass
+
+    def display_prev_day(self):
+        pass
+
+    def new_activity(self):
+        pass
 
 
 class AgendaTopMenuDropDown(DropDown):
@@ -30,6 +74,26 @@ class AgendaTopMenuDropDown(DropDown):
         self.auto_width = False
 
         self.app = app
+
+
+class SelectedDateDropDown(DropDown):
+    def __init__(self, app):
+        super(SelectedDateDropDown, self).__init__()
+
+        self.auto_width = False
+
+        self.app = app
+
+
+class AboutDialog(BoxLayout):
+    root = ObjectProperty(None)
+    dialog_content = '{} was made by {} for the Kivy App Contest 2014.'.format(__appname__, __author__)
+
+    def __init__(self, **kwargs):
+        super(AboutDialog, self).__init__(**kwargs)
+
+    def dismiss_dialog(self):
+        self.root.about_popup.dismiss()
 
 
 class AgendaApp(App):
@@ -43,6 +107,7 @@ class AgendaApp(App):
         EventLoop.ensure_window()
 
         self.window = EventLoop.window
+        self.title = __appname__
 
         return Agenda()
 
